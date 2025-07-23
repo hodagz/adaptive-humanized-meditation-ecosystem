297ca9425c5723a7d897b9ba98ed4f3e3b0a98c4
hodagz <118003349+hodagz@users.noreply.github.com>
2025-07-23 17:13:50 +0330

    Update get_refresh_token.py

--- get_refresh_token.py
+++ get_refresh_token.py
@@ -1,11 +1,17 @@
-import json
+# get_refresh_token.py
+
 from google_auth_oauthlib.flow import InstalledAppFlow
+import json
 
+# Load client secret
 with open("secrets/client_secret.json", "r") as f:
-    client_secrets = json.load(f)
+    client_config = json.load(f)
 
-flow = InstalledAppFlow.from_client_config(client_secrets, scopes=["https://www.googleapis.com/auth/youtube.upload"])
-creds = flow.run_console()
+flow = InstalledAppFlow.from_client_config(
+    client_config,
+    scopes=["https://www.googleapis.com/auth/youtube.upload"]
+)
 
+creds = flow.run_local_server(port=0)
 print("\n✅ Your refresh token:")
 print(creds.refresh_token)
